f442bbf73b3f4e55aaeba7a648066dbb3a146f74
rab-berlin <165693075+rab-berlin@users.noreply.github.com>
2024-04-06 07:25:13 +0200

    Update busch2090.ino
    Undocumented behaviour of the Microtronic 2090:
    Overflow during HXDZ leads to 0 in all registers D, E, and F.

--- busch2090/busch2090.ino
+++ busch2090/busch2090.ino
@@ -1618,12 +1618,20 @@ void run()
         zero = num > 999;
         carry = false;
 
-        num %= 1000;
+        if (zero) {
 
-        reg[0xD] = num % 10;
-        reg[0xE] = (num / 10) % 10;
-        reg[0xF] = (num / 100) % 10;
+	  reg[0xD] = 0;
+	  reg[0xE] = 0;
+	  reg[0xF] = 0;
 
+	} else {
+
+	  reg[0xD] = num % 10;
+	  reg[0xE] = ( num / 10 ) % 10;
+	  reg[0xF] = ( num / 100 ) % 10;
+
+	}
+	      
         break;
 
       case OP_DZHX:
